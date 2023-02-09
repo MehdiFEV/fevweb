d1818c995883eb91d5107176237bdfe77a655da2
MehdiFEV <124870629+MehdiFEV@users.noreply.github.com>
2023-02-09 10:52:48 +0100

    inscription.php

--- inscription.php
+++ inscription.php
@@ -1,10 +1,9 @@
 <?php 
-    if (isset($_POST['envoie'])){
-        if (!empty($_POST['pseudo']) AND !empty($_POST['mdp'])){
-
-        }else {
-            echo"Veuillez compléter tout les champs..."
-        }
+if (isset($_POST['envoie'])){
+    if (!empty($_POST['pseudo']) AND !empty($_POST['mdp'])){
+    }else {
+    echo"Veuillez compléter tout les champs..."
+    }
 }
 ?>
 <!DOCTYPE html>
