46f503848374cdd03189a4441d3dc2b16b517d8f
MehdiFEV <124870629+MehdiFEV@users.noreply.github.com>
2023-02-09 10:50:06 +0100

    inscription.php

--- inscription.php
+++ inscription.php
@@ -13,15 +13,14 @@
         <title>Système Inscription</title>
         <link rel="icon" type="image/x-icon" href="/img/logo.ico">
         <meta charset="utf-8">
-        <link rel="stylesheet" href="css/style.css">
+        <body>
+            <form method="POST" action="" align="center"> 
+                <input type="text" name="pseudo">
+                <br>
+                <input type="password" name="mdp">
+                <br/><br/>
+                <input type="submit" name="envoie">
+            </form>
+        </body>
     </head>
-    <body>
-        <form method="POST" action="" align="center"> 
-            <input type="text" name="">
-            <br>
-            <input type="password" name="mdp">
-            <br/><br/>
-            <input type="submit" name="envoie">
-        </form>
-    </body>
 </html>
