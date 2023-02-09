f0c541e8a217638b6116c591c05f64e5851063d5
MehdiFEV <124870629+MehdiFEV@users.noreply.github.com>
2023-02-09 11:22:16 +0100

    inscription_debugAll.php

--- inscription.php
+++ inscription.php
@@ -7,7 +7,7 @@ if (isset($_POST['envoie'])){
         $mdp = sha1($_POST['mdp']);
         $insertUser = $bdd->prepare('INSERT INTO users(pseudo, mdp)VALUES(?, ?)');
         $insertUser->execute(array($pseudo, $mdp));
-        $recupUser = $bdd->prepare('SELECT * FROM users WHERE pseudo = ? AND mdp = ?'),
+        $recupUser = $bdd->prepare('SELECT * FROM users WHERE pseudo = ? AND mdp = ?');
         $recupUser->execute(array($pseudo, $mdp));
         if ($recupUser->rowCount() > 0 ){
             $_SESSION['pseudo'] = $pseudo;
