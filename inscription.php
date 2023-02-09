1ca84c755426b071c0537ff5467995f2c76defe4
MehdiFEV <124870629+MehdiFEV@users.noreply.github.com>
2023-02-09 10:42:23 +0100

    inscription.php

--- inscription.php
+++ inscription.php
@@ -1,20 +1,19 @@
+<?php ?>
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Système Login</title>
+        <title>Système Inscription</title>
         <link rel="icon" type="image/x-icon" href="/img/logo.ico">
         <meta charset="utf-8">
         <link rel="stylesheet" href="css/style.css">
     </head>
     <body>
-        <form method="POST" action="">
-            <input type="text" name="pseudo">
+        <form method="POST" action=""> 
+            <input type="text" name="">
             <br>
             <input type="password" name="mdp">
-            <br><br>
-            <input type="submit" name="envoie"
-
-
+            <br/><br/>
+            <input type="submit" name="envoie">
         </form>
     </body>
 </html>
